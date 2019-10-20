d77ea88640c4874fff0718abf460e74761a009d7
Patrick Harrer <patrick.harrer94@hotmail.com>
2019-10-20 15:42:31 +0200

    converted to n variables

--- Test.m
+++ Test.m
@@ -6,9 +6,9 @@ format long
 %A Modified Sufficient Descent Polak–Ribiére–Polyak Type Conjugate Gradient Method for Unconstrained Optimization Problems
 
 [x,y] = meshgrid(-5:0.5:5,-5:0.5:5);
-z = func1(x,y);
+z = func2({x,y});
 
-[xmin,ymin] = min_cg(@func1, 3, 3, 10^-5, 0.1, 50);
+xmin = min_cg(@func1, [3;3], 10^-3, 0.1, 50);
 
 
 % figure()
@@ -18,38 +18,66 @@ z = func1(x,y);
 figure()
 hold on
 contour(x,y,z)
-plot(xmin,ymin,'-r*')
+plot(xmin(1,:),xmin(2,:),'-r*')
 title('contour')
 hold off
 
 
+%% 
 
-function z = func1(x,y)
+function z = func2(x)
 
-%[a,b,c,d,e,f] = args{:};
-z = 2*x.^2 + 3*x.*y + 7.*y.^2 + 8.*x + 9.*y + 10;
+% if ~ isvector(x)
+%     error("Input for function is not a Vektor")
+% end
+
+
+z = 2*x{1}.^2 + 3*x{1}.*x{2} + 7.*x{2}.^2 + 8.*x{1} + 9.*x{2} + 10;
 end
 
+function z = func1(x)
 
-function [xmin,ymin] = min_cg(fun, x0, y0, gtol, eps, maxiter)
+% if ~ isvector(x)
+%     error("Input for function is not a Vektor")
+% end
+
+
+z = 2*x(1,:).^2 + 3*x(1,:).*x(2,:) + 7.*x(2,:).^2 + 8.*x(1,:) + 9.*x(2,:) + 10;
+end
+%%
+
+function [xmin] = min_cg(fun, x0, gtol, eps, maxiter)
 
 k = 1;
-x(k) = x0;
-y(k) = y0;
+x(:,k) = x0;
+
 
 
 % Calculate Gradient at x0 y0
-df_dx(k) = diff([fun(x(k)-eps,y(k)),fun(x(k)+eps,y(k))])/(2 * eps);
-df_dy(k) = diff([fun(x(k),y(k)-eps),fun(x(k),y(k)+eps)])/(2 * eps);
+
+% generate matrix of function values for gradient calculation
+xvec = ones(1, length(x0))*x(1,k);
+for i = 2:length(x0)
+    xvec = [xvec; ones(1, length(x0))*x(i,k)];
+end
+
+xvec_eps1 = xvec - eye(length(x0))*eps;
+xvec_eps2 = xvec + eye(length(x0))*eps;
+
+
+% calculate function values
+fvec_eps1 = fun(xvec_eps1);
+fvec_eps2 = fun(xvec_eps2);
 
 % Gradient Vektor
-g{k} = [df_dx(k); df_dy(k)];
+g(:,k) = (fvec_eps2 - fvec_eps1)/(2*eps);
+
 
 % initial search direction
-d{k} = -g{k};
+d = -g;
 
 
-while norm(g{k}) > gtol
+while norm(g(:,k)) > gtol
     
     if k > maxiter
         disp('Maximum nubers of iteration reached')
@@ -62,45 +90,61 @@ while norm(g{k}) > gtol
     c = 0.5;
     tau = 0.9;
     
-    %m = g{k}'*(d{k}./norm(d{k}));
-    m = g{k}'*d{k};
+    m = g(:,k)'*d(:,k);
     
     t = -c*m;
     
     alpha(k) = 1;
     
-    while fun(x(k),y(k)) - fun(x(k)+alpha(k)*d{k}(1), y(k)+alpha(k)*d{k}(2)) < alpha(k)*t
+
+    while fun(x(:,k))-fun(x(:,k)+alpha(k)*d(:,k)) < alpha(k)*t
         alpha(k) = tau * alpha(k);
     end
+
     
     % Step 3: calculate new x
     
-    x(k+1) = x(k) + alpha(k)*d{k}(1);
-    y(k+1) = y(k) + alpha(k)*d{k}(2);
+    x(:,k+1) = x(:,k) + alpha(k)*d(:,k);
     
     % Step 4: calculate new serch direction
     
     mu= 0.5;
     
     % Calculate Gradient at xk+1 yk+1
-    df_dx(k+1) = diff([fun(x(k+1)-eps,y(k+1)),fun(x(k+1)+eps,y(k+1))])/(2 * eps);
-    df_dy(k+1) = diff([fun(x(k+1),y(k+1)-eps),fun(x(k+1),y(k+1)+eps)])/(2 * eps);
+%     df_dx(k+1) = diff([fun(x(k+1)-eps,y(k+1)),fun(x(k+1)+eps,y(k+1))])/(2 * eps);
+%     df_dy(k+1) = diff([fun(x(k+1),y(k+1)-eps),fun(x(k+1),y(k+1)+eps)])/(2 * eps);
+    
+    % generate matrix of function values for gradient calculation
+    xvec = ones(1, length(x0))*x(1,k+1);
+    for i = 2:length(x0)
+        xvec = [xvec; ones(1, length(x0))*x(i,k+1)];
+    end
+    
+    xvec_eps1 = xvec - eye(length(x0))*eps;
+    xvec_eps2 = xvec + eye(length(x0))*eps;
+    
+    
+    % calculate function values
+    fvec_eps1 = fun(xvec_eps1);
+    fvec_eps2 = fun(xvec_eps2);
     
     % Gradient Vektor
-    g{k+1} = [df_dx(k+1); df_dy(k+1)];
+    g(:,k+1) = (fvec_eps2 - fvec_eps1)/(2*eps);
+    
     
 
-    if norm(g{k+1}) < gtol
+    if norm(g(:,k+1)) < gtol
         disp(['Minimum found after ', num2str(k), ' iterations.'])
-        disp(['The minnimum is at ', num2str(x(k+1)),' ', num2str(y(k+1))])
+        disp('The minnimum is at ')
+        disp(x(:,k+1))
         break
     end
     
     % calculate beta
-    beta(k+1) = g{k+1}'*(g{k+1}-g{k})/max(mu*norm(d{k})*norm(g{k+1}-g{k}),norm(g{k})^2);
+    beta(k+1) = g(:,k+1)'*(g(:,k+1)-g(:,k))/max(mu*norm(d(:,k))*norm(g(:,k+1)-g(:,k)),norm(g(:,k))^2);
     
     % calculate new search direction
-    d{k+1} = -g{k+1}+beta(k+1)*d{k}-beta(k+1)*(g{k+1}'*d{k})/(g{k+1}'*(g{k+1}-g{k}))*(g{k+1}-g{k});
+    d(:,k+1) = -g(:,k+1)+beta(k+1)*d(:,k)-beta(k+1)*(g(:,k+1)'*d(:,k))/(g(:,k+1)'*(g(:,k+1)-g(:,k)))*(g(:,k+1)-g(:,k));
     
     % Step 5: Update k
     
@@ -110,6 +154,5 @@ end
 
 
 xmin=x;
-ymin=y;
 
 end

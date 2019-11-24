e4c84700823ceced88f61794369d892b772fc65b
Patrick Harrer <patrick.harrer94@hotmail.com>
2019-11-24 17:43:11 +0100

    added reaction

---
+++ A+B = C/dge3.m
@@ -0,0 +1,131 @@
+
+function F=dge3(x,T,x_3p)
+
+% x1' or one molefraction has to be specified
+
+alpha_12 = 0.2; 
+
+
+
+
+alpha_13 = 0.3;
+alpha_23 = 0.3;
+
+tau_12 = tau(T,12);
+tau_21 = tau(T,21);
+tau_13 = tau(T,13);
+tau_23 = tau(T,23);
+tau_31 = tau(T,31);
+tau_32 = tau(T,32);
+
+
+
+
+% definition of the 3 variabes
+x_1p = x(1,:);
+x_1dp = x(2,:);
+
+if length(x) == 3
+    x_3dp  = x(3,:);
+else
+    x_3dp = 0;
+end
+
+
+% elimination of the other variables with standardization operation
+x_2p = 1-x_1p-x_3p;
+x_2dp = 1-x_1dp-x_3dp;
+
+
+%NRTL Model for heptane (1), toluenee(2),sulfolane(3)
+G12 = exp(-alpha_12*tau_12);
+G21 = exp(-alpha_12*tau_21);
+G13 = exp(-alpha_13*tau_13);
+G23 = exp(-alpha_23*tau_23);
+G31 = exp(-alpha_13*tau_31);
+G32 = exp(-alpha_23*tau_32);
+
+G11 = 1;
+G22 = 1;
+G33 = 1;
+
+tau_11 = 0;
+tau_22 = 0;
+tau_33 = 0;
+
+% NRTL for activity cooeficients
+% theta_1 = (x1 + G21*x2 + G31*x3);
+% theta_2 = (x2 + G12*x1 + G32*x3);
+% theta_3 = (x3 + G13*x1 + G23*x2);
+% 
+% phi_1 = (x2*tau_21*G21 + x3*tau_31*G31);
+% phi_2 = (x1*tau_12*G12 + x3*tau_32*G32);
+% phi_3 = (x1*tau_13*G13 + x2*tau_23*G23);
+
+%ln_gamma_1p = (x_1p + G21*x_2p + G31*x_3p)/(x_2p*tau_21*G21 + x_3p*tau_31*G31) - x_1p*(x_1p + G21*x_2p + G31*x_3p)/(x_2p*tau_21*G21 + x_3p*tau_31*G31)^2+x_2p*G12/(x_1p*tau_12*G12 + x_3p*tau_32*G32)*(tau_12-(x_2p + G12*x_1p + G32*x_3p)/(x_1p*tau_12*G12 + x_3p*tau_32*G32))+x_3p*G13/(x_1p*tau_13*G13 + x_2p*tau_23*G23)*(tau_13-(x_3p + G13*x_1p + G23*x_2p)/(x_1p*tau_13*G13 + x_2p*tau_23*G23));
+%ln_gamma_2p = (x_2p + G12*x_1p + G32*x_3p)/(x_1p*tau_12*G12 + x_3p*tau_32*G32) - x_2p*(x_2p + G12*x_1p + G32*x_3p)/(x_1p*tau_12*G12 + x_3p*tau_32*G32)^2+x_1p*G21/(x_2p*tau_21*G21 + x_3p*tau_31*G31)*(tau_21-(x_1p + G21*x_2p + G31*x_3p)/(x_2p*tau_21*G21 + x_3p*tau_31*G31))+x_3p*G23/(x_1p*tau_13*G13 + x_2p*tau_23*G23)*(tau_23-(x_3p + G13*x_1p + G23*x_2p)/(x_1p*tau_13*G13 + x_2p*tau_23*G23));
+%ln_gamma_3p = (x_3p + G13*x_1p + G23*x_2p)/(x_1p*tau_13*G13 + x_2p*tau_23*G23) - x_3p*(x_3p + G13*x_1p + G23*x_2p)/(x_1p*tau_13*G13 + x_2p*tau_23*G23)^2+x_1p*G31/(x_2p*tau_21*G21 + x_3p*tau_31*G31)*(tau_31-(x_1p + G21*x_2p + G31*x_3p)/(x_2p*tau_21*G21 + x_3p*tau_31*G31))+x_2p*G32/(x_1p*tau_12*G12 + x_3p*tau_32*G32)*(tau_32-(x_2p + G12*x_1p + G32*x_3p)/(x_1p*tau_12*G12 + x_3p*tau_32*G32));
+
+%ln_gamma_1dp = (x_1dp + G21*x_2dp + G31*x_3dp)/(x_2dp*tau_21*G21 + x_3dp*tau_31*G31) - x_1dp*(x_1dp + G21*x_2dp + G31*x_3dp)/(x_2dp*tau_21*G21 + x_3dp*tau_31*G31)^2+x_2dp*G12/(x_1dp*tau_12*G12 + x_3dp*tau_32*G32)*(tau_12-(x_2dp + G12*x_1dp + G32*x_3dp)/(x_1dp*tau_12*G12 + x_3dp*tau_32*G32))+x_3dp*G13/(x_1dp*tau_13*G13 + x_2dp*tau_23*G23)*(tau_13-(x_3dp + G13*x_1dp + G23*x_2dp)/(x_1dp*tau_13*G13 + x_2dp*tau_23*G23));
+%ln_gamma_2dp = (x_2dp + G12*x_1dp + G32*x_3dp)/(x_1dp*tau_12*G12 + x_3dp*tau_32*G32) - x_2dp*(x_2dp + G12*x_1dp + G32*x_3dp)/(x_1dp*tau_12*G12 + x_3dp*tau_32*G32)^2+x_1dp*G21/(x_2dp*tau_21*G21 + x_3dp*tau_31*G31)*(tau_21-(x_1dp + G21*x_2dp + G31*x_3dp)/(x_2dp*tau_21*G21 + x_3dp*tau_31*G31))+x_3dp*G23/(x_1dp*tau_13*G13 + x_2dp*tau_23*G23)*(tau_23-(x_3dp + G13*x_1dp + G23*x_2dp)/(x_1dp*tau_13*G13 + x_2dp*tau_23*G23));
+%ln_gamma_3dp = (x_3dp + G13*x_1dp + G23*x_2dp)/(x_1dp*tau_13*G13 + x_2dp*tau_23*G23) - x_3dp*(x_3dp + G13*x_1dp + G23*x_2dp)/(x_1dp*tau_13*G13 + x_2dp*tau_23*G23)^2+x_1dp*G31/(x_2dp*tau_21*G21 + x_3dp*tau_31*G31)*(tau_31-(x_1dp + G21*x_2dp + G31*x_3dp)/(x_2dp*tau_21*G21 + x_3dp*tau_31*G31))+x_2dp*G32/(x_1dp*tau_12*G12 + x_3dp*tau_32*G32)*(tau_32-(x_2dp + G12*x_1dp + G32*x_3dp)/(x_1dp*tau_12*G12 + x_3dp*tau_32*G32));
+
+
+
+
+ln_gamma_1p = (x_1p.*tau_11.*G11+x_2p.*tau_21.*G21+x_3p.*tau_31.*G31)./(x_1p.*G11+x_2p.*G21+x_3p.*G31)+ ...
+              (x_1p.*G11)./(x_1p.*G11+x_2p.*G21+x_3p.*G31).*(tau_11-(x_1p.*tau_11.*G11+x_2p.*tau_21.*G21+x_3p.*tau_31.*G31)./(x_1p.*G11+x_2p.*G21+x_3p.*G31))+ ...
+              (x_2p.*G12)./(x_1p.*G12+x_2p.*G22+x_3p.*G32).*(tau_12-(x_1p.*tau_12.*G12+x_2p.*tau_22.*G22+x_3p.*tau_32.*G32)./(x_1p.*G12+x_2p.*G22+x_3p.*G32))+ ...
+              (x_3p.*G13)./(x_1p.*G13+x_2p.*G23+x_3p.*G33).*(tau_13-(x_1p.*tau_13.*G13+x_2p.*tau_23.*G23+x_3p.*tau_33.*G33)./(x_1p.*G13+x_2p.*G23+x_3p.*G33));
+          
+ln_gamma_2p = (x_1p.*tau_12.*G12+x_2p.*tau_22.*G22+x_3p.*tau_32.*G32)./(x_1p.*G12+x_2p.*G22+x_3p.*G32)+ ...
+              (x_1p.*G21)./(x_1p.*G11+x_2p.*G21+x_3p.*G31).*(tau_21-(x_1p.*tau_11.*G11+x_2p.*tau_21.*G21+x_3p.*tau_31.*G31)./(x_1p.*G11+x_2p.*G21+x_3p.*G31))+ ...
+              (x_2p.*G22)./(x_1p.*G12+x_2p.*G22+x_3p.*G32).*(tau_22-(x_1p.*tau_12.*G12+x_2p.*tau_22.*G22+x_3p.*tau_32.*G32)./(x_1p.*G12+x_2p.*G22+x_3p.*G32))+ ...
+              (x_3p.*G23)./(x_1p.*G13+x_2p.*G23+x_3p.*G33).*(tau_23-(x_1p.*tau_13.*G13+x_2p.*tau_23.*G23+x_3p.*tau_33.*G33)./(x_1p.*G13+x_2p.*G23+x_3p.*G33));
+          
+ln_gamma_3p = (x_1p.*tau_13.*G13+x_2p.*tau_23.*G23+x_3p.*tau_33.*G33)./(x_1p.*G13+x_2p.*G23+x_3p.*G33)+ ...
+              (x_1p.*G31)./(x_1p.*G11+x_2p.*G21+x_3p.*G31).*(tau_31-(x_1p.*tau_11.*G11+x_2p.*tau_21.*G21+x_3p.*tau_31.*G31)./(x_1p.*G11+x_2p.*G21+x_3p.*G31))+ ...
+              (x_2p.*G32)./(x_1p.*G12+x_2p.*G22+x_3p.*G32).*(tau_32-(x_1p.*tau_12.*G12+x_2p.*tau_22.*G22+x_3p.*tau_32.*G32)./(x_1p.*G12+x_2p.*G22+x_3p.*G32))+ ...
+              (x_3p.*G33)./(x_1p.*G13+x_2p.*G23+x_3p.*G33).*(tau_33-(x_1p.*tau_13.*G13+x_2p.*tau_23.*G23+x_3p.*tau_33.*G33)./(x_1p.*G13+x_2p.*G23+x_3p.*G33));
+          
+
+          
+ln_gamma_1dp = (x_1dp.*tau_11.*G11+x_2dp.*tau_21.*G21+x_3dp.*tau_31.*G31)./(x_1dp.*G11+x_2dp.*G21+x_3dp.*G31)+ ...
+              (x_1dp.*G11)./(x_1dp.*G11+x_2dp.*G21+x_3dp.*G31).*(tau_11-(x_1dp.*tau_11.*G11+x_2dp.*tau_21.*G21+x_3dp.*tau_31.*G31)./(x_1dp.*G11+x_2dp.*G21+x_3dp.*G31))+ ...
+              (x_2dp.*G12)./(x_1dp.*G12+x_2dp.*G22+x_3dp.*G32).*(tau_12-(x_1dp.*tau_12.*G12+x_2dp.*tau_22.*G22+x_3dp.*tau_32.*G32)./(x_1dp.*G12+x_2dp.*G22+x_3dp.*G32))+ ...
+              (x_3dp.*G13)./(x_1dp.*G13+x_2dp.*G23+x_3dp.*G33).*(tau_13-(x_1dp.*tau_13.*G13+x_2dp.*tau_23.*G23+x_3dp.*tau_33.*G33)./(x_1dp.*G13+x_2dp.*G23+x_3dp.*G33));
+          
+ln_gamma_2dp = (x_1dp.*tau_12.*G12+x_2dp.*tau_22.*G22+x_3dp.*tau_32.*G32)./(x_1dp.*G12+x_2dp.*G22+x_3dp.*G32)+ ...
+              (x_1dp.*G21)./(x_1dp.*G11+x_2dp.*G21+x_3dp.*G31).*(tau_21-(x_1dp.*tau_11.*G11+x_2dp.*tau_21.*G21+x_3dp.*tau_31.*G31)./(x_1dp.*G11+x_2dp.*G21+x_3dp.*G31))+ ...
+              (x_2dp.*G22)./(x_1dp.*G12+x_2dp.*G22+x_3dp.*G32).*(tau_22-(x_1dp.*tau_12.*G12+x_2dp.*tau_22.*G22+x_3dp.*tau_32.*G32)./(x_1dp.*G12+x_2dp.*G22+x_3dp.*G32))+ ...
+              (x_3dp.*G23)./(x_1dp.*G13+x_2dp.*G23+x_3dp.*G33).*(tau_23-(x_1dp.*tau_13.*G13+x_2dp.*tau_23.*G23+x_3dp.*tau_33.*G33)./(x_1dp.*G13+x_2dp.*G23+x_3dp.*G33));
+          
+ln_gamma_3dp = (x_1dp.*tau_13.*G13+x_2dp.*tau_23.*G23+x_3dp.*tau_33.*G33)./(x_1dp.*G13+x_2dp.*G23+x_3dp.*G33)+ ...
+              (x_1dp.*G31)./(x_1dp.*G11+x_2dp.*G21+x_3dp.*G31).*(tau_31-(x_1dp.*tau_11.*G11+x_2dp.*tau_21.*G21+x_3dp.*tau_31.*G31)./(x_1dp.*G11+x_2dp.*G21+x_3dp.*G31))+ ...
+              (x_2dp.*G32)./(x_1dp.*G12+x_2dp.*G22+x_3dp.*G32).*(tau_32-(x_1dp.*tau_12.*G12+x_2dp.*tau_22.*G22+x_3dp.*tau_32.*G32)./(x_1dp.*G12+x_2dp.*G22+x_3dp.*G32))+ ...
+              (x_3dp.*G33)./(x_1dp.*G13+x_2dp.*G23+x_3dp.*G33).*(tau_33-(x_1dp.*tau_13.*G13+x_2dp.*tau_23.*G23+x_3dp.*tau_33.*G33)./(x_1dp.*G13+x_2dp.*G23+x_3dp.*G33));
+
+          
+F(1,:) = x_1p.*exp(ln_gamma_1p)-x_1dp.*exp(ln_gamma_1dp);    
+F(2,:) = x_2p.*exp(ln_gamma_2p)-x_2dp.*exp(ln_gamma_2dp);
+
+if length(x) == 3
+    F(3,:) = x_3p.*exp(ln_gamma_3p)-x_3dp.*exp(ln_gamma_3dp);
+    K = 2;
+    %F(3,:) = K.*(x_1p.*exp(ln_gamma_1p).*x_2p.*exp(ln_gamma_2p))-(x_3dp.*exp(ln_gamma_3dp));
+    %F(3,:) = x_2p.*exp(ln_gamma_2p)./(K.*x_1p.*exp(ln_gamma_1p))-(x_3dp.*exp(ln_gamma_3dp));
+end
+
+end
+
+
+
+
+
+  
+   
+
+
+
+
+
---
+++ A+B = C/reactive_LLE3.m
@@ -0,0 +1,125 @@
+% Model development
+% WS 2019
+%
+%
+%
+% Reaktive LLE
+
+% comp1: Hexane
+% comp2: Methanol
+% comp3: Chloroform
+
+% phase 1 : a
+% pahse 2 : b
+
+
+clear variables; 
+clc;
+close all;
+format long;
+options = optimoptions('fsolve','StepTolerance',1e-10);
+%%
+%% calculation LLE
+
+% critical Temperatur and composition for the binary system 
+alpha12 = 0.2;
+
+%c=fsolve(@(x)critVal(x,alpha12),[0.1;283],options);
+%min_cg(fun, x0, gtol, eps, maxiter, rho, delta, mu)
+c=min_cg_new(@(x)target_crit(x,alpha12),[0.5;283],10^-8, 10^-6, 10^6, 0.2, 10^-2, 10^-1);
+
+T_crit = c(2);
+x_crit = c(1);
+
+
+
+
+%% LLE diagram ternary system
+x_3a(1) = 0; % one x has to be define in order to solve the equations
+
+% initial values
+x_1a(1) = 0.2;
+x_1b(1) = 0.75;
+x_3b(1) = 0;
+
+T_calc = 298.15;
+rho = 0.3;
+
+for i=1:32
+        if i==1   
+        h=fsolve(@(x)dge3(x,T_calc,x_3a(i)),[x_1a(i);x_1b(i);x_3b(i)],options); % starting values for x1', x1'',x3''
+        %h=min_cg_new(@(x)target(x,T_calc,x_3a(i)),[x_1a(i);x_1b(i);x_3b(i)], 10^-7, 10^-5, 10^6, rho, 10^-2, 10^-1);
+        else
+            h=fsolve(@(x)dge3(x,T_calc,x_3a(i)),[x_1a(i-1);x_1b(i-1);x_3b(i-1)],options); % starting values for x1'',x2'',x3'
+            %h=min_cg_new(@(x)target(x,T_calc,x_3a(i)),[x_1a(i-1);x_1b(i-1);x_3b(i-1)], 10^-7, 10^-5, 10^6, rho, 10^-2, 10^-1);
+        end
+        
+        x_1a(i) = h(1);
+        x_1b(i) = h(2);
+        x_3b(i)  = h(3) ;
+        
+        x_2a(i) = 1-x_1a(i)-x_3a(i);
+        x_2b(i) = 1-x_1b(i)-x_3b(i);
+
+        
+        x_3a(i+1) = x_3a(i)+0.01;
+        
+
+
+        if and(x_1a(i)==0, x_1b(i)==0)
+            break
+        end
+       
+       
+         
+end
+
+   x_3a(end) = [];
+   
+   %tie Lines
+   X = [x_1a(1:3:end);x_1b(1:3:end)];
+   Y = [x_2a(1:3:end);x_2b(1:3:end)];
+   Z = [x_3a(1:3:end);x_3b(1:3:end)];
+   
+   
+   figure('Position', [50,50,1000,800]);
+   
+%       chloroform
+%          /  \
+%         /    \
+%   hexane --- methanol
+   
+    ternplot(x_2a,x_3a,x_1a, 'r.-', 'majors', 10); 
+    hold on
+    ternplot(x_2b,x_3b,x_1b, 'b.-', 'majors', 10);
+    
+    ternplot(Y,Z,X, 'go-');
+    
+    title('Ternary Diagram with Reaction', 'Position',[0.1 0.9])
+    ternlabel('B','C','A'); 
+    legend('Phase I', 'Phase II', 'Tie Lines')
+   
+%% Target function 
+    
+function t = target(x,T,x_3a)
+
+    z = dge3(x,T,x_3a);
+    z1 = z(1,:);
+    z2 = z(2,:);
+    z3 = z(3,:);
+    t = z1.^2 + z2.^2 + z3.^2;
+
+end
+
+function t = target_crit(x,alpha_12)
+
+    z = critVal(x,alpha_12);
+    z1 = z(1,:);
+    z2 = z(2,:);
+    t = z1.^2 + z2.^2;
+
+end  
+  
+   
+   
+  
